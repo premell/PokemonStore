cf3de654b248a76f7265ea65248862eb48a71b40
premell <premell@protonmail.com>
2021-09-18 17:27:44 +0000

    added all 2000 pokemon back for testing

--- pages/pokemon/[pokemonName]/index.js
+++ pages/pokemon/[pokemonName]/index.js
@@ -12,7 +12,7 @@ const pokemon = ({ pokemon }) => {
 
 export const getStaticPaths = async () => {
   const defaultPokemonRefs = await fetchData(
-    BASE_URL + "pokemon?offset=0&limit=100"
+    BASE_URL + "pokemon?offset=0&limit=2000"
   );
   console.log(defaultPokemonRefs);
 
